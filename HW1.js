fdd4720dc66bc891a146efd62e330a770bb5926a
Jane Tian <tiany2@seattleu.edu>
2017-10-03 00:25:43 -0700

    Update xml and js data

---
+++ HW1.js
@@ -0,0 +1,118 @@
+{
+  "CATALOG": {
+    "CD": [
+      {
+        "TITLE": "Anna Bai",
+        "ARTIST": "Marketer",
+        "COUNTRY": "Redmond",
+        "COMPANY": "113",
+        "PRICE": "35998",
+        "YEAR": "65000"
+      },
+      {
+        "TITLE": "Jimmy Lam",
+        "ARTIST": "Sales",
+        "COUNTRY": "Kirkland",
+        "COMPANY": "456",
+        "PRICE": "36008",
+        "YEAR": "60000"
+      },
+      {
+        "TITLE": "Linda Koveski",
+        "ARTIST": "Program Manager",
+        "COUNTRY": "Seattle",
+        "COMPANY": "235",
+        "PRICE": "36237",
+        "YEAR": "110000"
+      },
+      {
+        "TITLE": "Tim Hakuddo ",
+        "ARTIST": "Developer",
+        "COUNTRY": "Renton",
+        "COMPANY": "464",
+        "PRICE": "36919",
+        "YEAR": "100000"
+      },
+      {
+        "TITLE": "Adam Ladyman",
+        "ARTIST": "Designer",
+        "COUNTRY": "Bellevue",
+        "COMPANY": "958",
+        "PRICE": "36965",
+        "YEAR": "70000"
+      },
+      {
+        "TITLE": "Doris Ke",
+        "ARTIST": "Support",
+        "COUNTRY": "San Francisco",
+        "COMPANY": "234",
+        "PRICE": "37259",
+        "YEAR": "55000"
+      },
+      {
+        "TITLE": "Ivy Sherman",
+        "ARTIST": "Operation",
+        "COUNTRY": "Woodinville",
+        "COMPANY": "452",
+        "PRICE": "38857",
+        "YEAR": "63000"
+      },
+      {
+        "TITLE": "linsey Miva",
+        "ARTIST": "Sales",
+        "COUNTRY": "Renton",
+        "COMPANY": "755",
+        "PRICE": "38859",
+        "YEAR": "60000"
+      },
+      {
+        "TITLE": "Carson Woodin",
+        "ARTIST": "Program Manager",
+        "COUNTRY": "Bellevue",
+        "COMPANY": "123",
+        "PRICE": "39661",
+        "YEAR": "110000"
+      },
+      {
+        "TITLE": "Kirk Amala",
+        "ARTIST": "Developer",
+        "COUNTRY": "San Francisco",
+        "COMPANY": "525",
+        "PRICE": "39734",
+        "YEAR": "100000"
+      },
+      {
+        "TITLE": "Kim Lee",
+        "ARTIST": "Designer",
+        "COUNTRY": "Woodinville",
+        "COMPANY": "145",
+        "PRICE": "39753",
+        "YEAR": "70000"
+      },
+      {
+        "TITLE": "Wani Love",
+        "ARTIST": "Support",
+        "COUNTRY": "Chicago",
+        "COMPANY": "640",
+        "PRICE": "39849",
+        "YEAR": "55000"
+      },
+      {
+        "TITLE": "Sindney Jama",
+        "ARTIST": "Marketer",
+        "COUNTRY": "Vancouver",
+        "COMPANY": "582",
+        "PRICE": "39984",
+        "YEAR": "63000"
+      },
+      {
+        "TITLE": "Willliam Law",
+        "ARTIST": "Sales",
+        "COUNTRY": "Cleveland",
+        "COMPANY": "897",
+        "PRICE": "40398",
+        "YEAR": "60000"
+      }
+    ]
+  }
+}
